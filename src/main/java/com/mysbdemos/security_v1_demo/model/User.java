ec110e7ddeb5d96b5c65bbe264eefc752c342918
talasbaev1989@gmail.com <talasbaev1989@gmail.com>
2021-01-29 22:02:42 +0600

    added sql scripts

--- src/main/java/com/mysbdemos/security_v1_demo/model/User.java
+++ src/main/java/com/mysbdemos/security_v1_demo/model/User.java
@@ -1,8 +1,19 @@
 package com.mysbdemos.security_v1_demo.model;
 
+import lombok.*;
+
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "")
 public class User extends BaseEntity {
 
     @Id
---
+++ src/main/resources/db.migration/V0.1_test_data.sql
@@ -0,0 +1,52 @@
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE OR REPLACE FUNCTION bytea_import(p_path TEXT, p_result OUT BYTEA)
+    LANGUAGE plpgsql AS
+$$
+DECLARE
+    l_oid OID;
+BEGIN
+    SELECT lo_import(p_path) INTO l_oid;
+    SELECT lo_get(l_oid) INTO p_result;
+    PERFORM lo_unlink(l_oid);
+END;
+$$;
+
+DO
+$BODY$
+    DECLARE
+        user_1_uuid    uuid        := uuid_generate_v5();
+        user_2_uuid    uuid        := uuid_generate_v4();
+        user_3_uuid    uuid        := uuid_generate_v4();
+        user_1_email   varchar(16) := 'owner1@email.com';
+        user_2_email   varchar(16) := 'owner2@email.com';
+        user_3_email   varchar(16) := 'user1@email.com';
+        admin_role_id  int         := nextval('auth.role_id_seq'::regclass);
+        user_role_id   int         := nextval('auth.role_id_seq'::regclass);
+        viewer_role_id int         := nextval('auth.role_id_seq'::regclass);
+
+    BEGIN
+        INSERT INTO auth.users(id, email, first_name, last_name, password, avatar_path, is_enabled)
+        VALUES (user_1_uuid, user_1_email, 'Ludwig', 'Beethoven',
+                concat('{bcrypt}', crypt('owner1', gen_salt('bf', 8))), null, true),
+
+               (user_2_uuid, user_2_email, 'Johann', 'Sebastian',
+                concat('{bcrypt}', crypt('owner2', gen_salt('bf', 8))), null, true),
+
+               (user_3_uuid, user_3_email, 'Johannes', 'Brahms',
+                concat('{bcrypt}', crypt('user1', gen_salt('bf', 8))), null, true);
+
+        INSERT INTO auth.roles(id, name)
+        VALUES (admin_role_id, 'ROLE_ADMIN'),
+               (user_role_id, 'ROLE_USER'),
+               (viewer_role_id, 'ROLE_DOCUMENT_VIEWER');
+
+    END;
+$BODY$ LANGUAGE plpgsql;
